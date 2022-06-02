a7b89f1f249385d578415e4fdfee4f5dd1d4fa9c
Mehdi <mehdiatmani@icloud.com>
2022-06-02 20:54:12 +0300

    Updated version, with second GUI, just before trying the GUI from internet
    Still working on it
    
    More and more advancements 2 june 2022

--- src/com/company/Door.java
+++ src/com/company/Door.java
@@ -1,26 +1,23 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.text.JTextComponent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import javax.swing.JOptionPane;
 
 public class Door extends DbConnection{
-    JPanel Window;
+    JPanel doorframe;
     private JButton SignB;
     private JButton LogB;
     private JTextField textField1;
-    private JTextField textField2;
-    private DbConnection db = new DbConnection();
-    private Main function = new Main();
+    private JPasswordField passwordField1;
+    private JButton forgotPasswordButton;
 
+    private DbConnection db = new DbConnection();
 
     public Door() {
 
-        textField2.addActionListener(new ActionListener() {
+        passwordField1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -38,18 +35,36 @@ public class Door extends DbConnection{
         SignB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String Password = textField1.getText();
-                String UserName = textField2.getText();
+                String Password = passwordField1.getText();
+                String UserName = textField1.getText();
+
+
+            }
+        });
+
+        LogB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String Password = passwordField1.getText();
+                String UserName = textField1.getText();
 
                 if (!Password.equals(null) && !UserName.equals(null) ){
 
                     db.insert(Password,UserName);
                     db.output();
                 }
-                else {
+                if(!Password.equals(null) && !UserName.equals(null) ) {
                     JOptionPane.showMessageDialog(null, "Error: You must fill all the data");
 
                 }
+
+            }
+        });
+
+        forgotPasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
             }
         });
@@ -59,11 +74,11 @@ public class Door extends DbConnection{
         // TODO: place custom component creation code here
     }
 
-//db.insert(textField2.getText(), textField1.getText() );
+
 public static void main(String[] args ) {
     Door x = new Door();//Name of class
     JFrame mainFrame = new JFrame("Door");
-    mainFrame.setContentPane(x.Window);//Panel
+    mainFrame.setContentPane(x.doorframe);//Panel
     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     mainFrame.pack();
     mainFrame.setVisible(true);
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -3,7 +3,15 @@ package com.company;
 import javax.swing.*;
 
 public class Menu {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JLabel User_Name_Menu;
+    private JLabel Password_Menu;
+    private JButton signInButton;
+    private JTextField textField2;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
